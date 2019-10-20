a1d561c7c7ecece2cfaf92d3423ee6911a86ae53
Aex <spryti@qq.com>
2019-10-20 21:07:08 +0800

    增加应用初始化后的回调

--- src/serve/index.ts
+++ src/serve/index.ts
@@ -1,19 +1,21 @@
 import * as Koa from 'koa'
-import { action, Dic } from '..'
+import { _, action, Dic } from '..'
 import life from '../life'
 import lib from './lib'
 import middleware from './middleware'
 
-export default async (option: { base?: string, apps: Dic<string> }) => {
+export default async (opt: { base?: string, apps: Dic<string>, started?: () => void }) => {
 
   lib.showBootInfo()
+
+  const option = _.defaults(opt, { base: '', app: {}, started: _.noop })
 
   const koa = new Koa()
 
   await life.created()
 
   // 初始化路由
-  action.attach(option.base || '', option.apps)
+  action.attach(option.base, option.apps)
 
   // 初始化koa中间件
   middleware(koa)
@@ -21,6 +23,7 @@ export default async (option: { base?: string, apps: Dic<string> }) => {
   const port = parseInt(process.env.PORT as string) || 8000
   koa.listen(port, async () => {
     await life.started()
+    option.started()
     lib.showBootInfo(port)
   })
 
