e89ea246c18511f8ffdfcf04f60b035bfa152dea
Aex <spryti@qq.com>
2020-01-03 01:00:33 +0800

    新增复杂多级扩展逻辑

--- src/coa-data/index.ts
+++ src/coa-data/index.ts
@@ -3,33 +3,51 @@ import { _, Dic } from '..'
 declare type TWorkData = Dic<any>;
 declare type TWorker = (ids: string[]) => Promise<TWorkData>
 
+function getIds (list: any[], keys: string[]) {
+  let ids = [] as string[]
+  _.forEach(list, v => {
+    if (typeof v === 'string')
+      return ids.push(v)
+    const item = _.get(v, keys[0]) as any
+    if (typeof item === 'string')
+      ids.push(item)
+    else if (_.isArray(item))
+      ids = _.concat(ids, getIds(item, keys.slice(1)))
+  })
+  return ids
+}
+
+function setValues (list: any, data: any, keys: string[], extend = '', value = {}) {
+  const values = [] as any[]
+  _.forEach(list, v => {
+    if (typeof v === 'string')
+      return values.push(data[v] || value)
+    const item = _.get(v, keys[0]) as any
+    if (typeof item === 'string') {
+      const newData = data[item] || value
+      extend ? _.set(v, extend, newData) : _.assign(v, newData)
+    } else if (_.isArray(item)) {
+      const newArray = setValues(item, data, keys.slice(1), extend, value)
+      newArray.length && _.set(v, extend, newArray)
+    }
+  })
+  return values
+}
+
 export default new class {
 
   // 扩展数据
   async extend (list: any[], key: string, extend = '', worker: TWorker, value = {}) {
 
+    const keys = key.split('->')
+
     // 获取需要的ID列表
-    const ids = [] as string[]
-    _.forEach(list, v => {
-      const id = _.get(v, key)
-      if (!id) return
-      if (_.isArray(id)) id.forEach(i => ids.push(i))
-      else ids.push(id)
-    })
+    const ids = getIds(list, keys)
 
     // 根据ID列表通过worker获取数据
     const data = ids.length < 1 ? {} : await worker(ids)
 
     // 遍历数据，将数据附加到list
-    _.forEach(list, v => {
-      const id = _.get(v, key)
-      if (_.isArray(id)) {
-        const newArray = id.map(v => data[v] || value)
-        _.set(v, extend, newArray)
-      } else {
-        const newData = data[id] || value
-        extend ? _.set(v, extend, newData) : _.assign(v, newData)
-      }
-    })
+    setValues(list, data, keys, extend, value)
   }
 }
