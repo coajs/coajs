c3afa48406d9a367f51ea6c02dfc749fa1278690
Aex <spryti@qq.com>
2020-03-14 16:15:54 +0800

    优化echo的color服务

--- src/coa-echo/index.ts
+++ src/coa-echo/index.ts
@@ -1,39 +1,39 @@
-import * as colors from 'colors/safe'
 import * as util from 'util'
+import colors from './colors'
 
 const tty = process.stdout.isTTY
 
 export default new class {
   log (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.black(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('black', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   info (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.magenta(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('magenta', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   error (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.red(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('red', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   warn (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.yellow(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('yellow', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   grey (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.gray(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('gray', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   green (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.green(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('green', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   blue (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.blue(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('blue', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   cyan (message: any = '', ...optionalParams: any[]) {
-    tty ? console.log(colors.cyan(util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
+    tty ? console.log(colors('cyan', util.format(message, ...optionalParams))) : console.log(message, ...optionalParams)
   }
 
   colors () {
